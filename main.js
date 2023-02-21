efc0cf650f88bfbb64a5a533781d10fca8e997b8
munsangu <ds-3632@hanmail.net>
2023-02-22 08:28:07 +0900

    20230222

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
-let hTag = document.querySelector("#title");
-const http = require("http");
-const { parse } = require("querystring");
+// let hTag = document.querySelector("#title");
+import { createServer } from "http";
+import { parse } from "querystring";
 
-http
-  .createServer(function (req, res) {
-    if (req.method === "POST" && req.url === "/uuid") {
-      let body = "";
-      req.on("data", (chunk) => {
-        body += chunk.toString();
-      });
-      req.on("end", () => {
-        const uuid = parse(body).uuid;
-        alert(`iOS 기기의 UUID: ${uuid}`);
+createServer(function (req, res) {
+  if (req.method === "POST" && req.url === "/uuid") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+    req.on("end", () => {
+      const uuid = parse(body).uuid;
+      alert(`iOS 기기의 UUID: ${uuid}`);
 
-        res.end("UUID를 받았음");
-      });
-    }
-  })
-  .listen(3000);
+      res.end("UUID를 받았음");
+    });
+  }
+}).listen(3000);
